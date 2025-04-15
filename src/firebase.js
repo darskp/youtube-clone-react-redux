29229dfdb37b6758683c9fec07c1d9f347c776ef
darskp <darshanpatgar100@gmail.com>
2025-04-15 23:15:40 +0530

    updated env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
-import {getStorage} from 'firebase/storage'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC6nR_TlVPuP-2Z9wwN__psJ9sd37TLRFY",
-  authDomain: "clone-778be.firebaseapp.com",
-  projectId: "clone-778be",
-  storageBucket: "clone-778be.appspot.com",
-  messagingSenderId: "501063407868",
-  appId: "1:501063407868:web:bd76039a02d5e8ce529f79"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-let auth=getAuth(app);
-let db=getFirestore(app);
-let storage=getStorage(app);
-export default app;
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export {auth,db,storage};
+export default app;
+export { auth, db, storage };
